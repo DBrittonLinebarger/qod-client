9d4fe69e0520588e5831a67226c1331872324b06
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-07-09 09:27:44 -0600

    added additional code

--- app/src/main/java/edu/cnm/deepdive/qodclient/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/qodclient/viewmodel/MainViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.Lifecycle.Event;
+import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
@@ -14,7 +15,7 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 import java.util.List;
 
-public class MainViewModel extends AndroidViewModel {
+public class MainViewModel extends AndroidViewModel implements LifecycleObserver {
 
   private MutableLiveData<Quote> random;
   private CompositeDisposable pending = new CompositeDisposable();
